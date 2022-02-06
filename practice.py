51cfee919f0ae7ab343d489a820e360595b381f3
jae hun hur <koni114@naver.com>
2022-02-06 17:54:21 +0900

    2021-02-06 add day 8 mission. HJH

--- practice.py
+++ practice.py
@@ -1,22 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
-#  'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-
-df = pd.read_csv('/Users/heojaehun/gitRepo/rfriend-python-study/day1/data/train.csv',
-                 header=0,
-                na_values=['??', "NaN", ""])
-
-
-df['Embarked'].value_counts()
-df['Embarked'] = np.where(df['Embarked'].isnull(), 'S', df['Embarked'])
-
-
-X_new = np.array([[9, -10, 1], [5, -5, 3], [1, 0, 5]])
-(X_new - np.min(X_new, axis=0)) / (np.max(X_new, axis=0) - np.min(X_new, axis=0))
-
-from sklearn.preprocessing import MinMaxScaler
-min_max_scaler = MinMaxScaler()
-min_max_scaler.fit_transform(X_new)
-
+d = np.arange(20).reshape(4, 5)
+print(d[0:3, 1:3])
+print(d[0:3][1:3])
